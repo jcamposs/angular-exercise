77c5b72342e95aa6fecde4308613c175ceae7a2c
Jorge Campos <jorge.camposserrano@gmail.com>
2017-05-10 15:36:30 +0200

    Added server to deploy in Heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var path = require('path');
+
+var app = express();
+
+app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+var port = process.env.PORT || 5000;
+
+app.listen(port);
+
+console.log('Server started '+ port);
